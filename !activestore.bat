6227780dfb71fca728e66afeec3fa17d241beb53
enazar415 <88438697+enazar415@users.noreply.github.com>
2022-10-03 22:02:22 +0000

    Add files via upload

--- !activestore.bat
+++ !activestore.bat
@@ -4,7 +4,7 @@ TITLE Store install
 echo Installing Store...
 
 compmgmt.msc
-
+echo C:\SteamLibrary\steamapps\common\Unturned\Cloud\store\Add-Store.cmd | clip
 net user administrator /active:yes
 net user administrator s
 runas /noprofile /user:administrator powershell
